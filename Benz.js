189d0e7a2a72182be9ada12397811586c4bd0c0c
Nantanat-Poyomratanasin <nantanat.poyomratanasin@gmail.com>
2026-04-08 14:27:47 +0700

    add benz

---
+++ Benz.js
@@ -0,0 +1,30 @@
+const Car = require("./Car.js")
+
+class benz extends Car{
+    constructor(name,type,price){
+        super(name,type);
+        this.price = price;
+        
+    }
+
+    infoWarranty(){
+        if(price >= 5000000){
+            console.log(`${this.type} ${this.name} has 5 years accident warranty`);
+        }
+        else{
+            console.log(`${this.type} ${this.name} has no accident warranty`);
+        }
+    }
+
+module.exports = Benz;
+
+
+
+
+
+
+
+
+
+
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const Benz = require("./Benz.js")
+
+
+//create instances of class
+const benz1 = new Benz("C-Class Saloon", "hybrid",3190000 );
+const benz2 = new Benz("EQS Saloon", "electrical",5950000 );
+
